79c7c31b9fe991164d272224b6f4d7eb42d55d8a
Tor Einar Lyngset <lyngset@gmail.com>
2013-08-18 17:14:57 +0200

    matrix scales to window width and height

--- www/js/game/ViewManager.js
+++ www/js/game/ViewManager.js
@@ -19,12 +19,58 @@ define(['jquery', 'handlebars', 'game/Resources'], function($, Handlebars, Resou
 
             templateElement.parent().html(html);
 
+            this.setupView();
+        },
+
+        setupView: function() {
+            this.setupOnImageClick();
+            this.scaleView();
+        },
+
+        setupOnImageClick: function() {
             var that = this;
             $('#image-matrix td').on('click', function() {
                 var id = $(this).attr('id');
                 var resource = that.resources.findByName(id);
                 resource.sound.play();
             });
+        },
+
+        /**
+         * Scales view based on window width / height
+         */
+        scaleView: function() {
+
+            var ratio = 1.5; // width/height ratio of image
+            var totalBorderWidth = 4 * 10;
+
+            var viewPort = {
+                width: $(window).width(),
+                height: $(window).height()
+            };
+
+            console.log(viewPort);
+
+            var matrixHeight = 0;
+            var matrixWidth = 0;
+
+            if ((viewPort.width / viewPort.height) >= ratio) { // Matrix is wider than ratio -> height is the deciding factor
+                var matrixHeight = viewPort.height - totalBorderWidth;
+                var matrixWidth = (matrixHeight * ratio);
+            }
+            else { // Matrix is higher than ratio -> width is the deciding factor
+                var matrixWidth = (viewPort.width - totalBorderWidth);
+                var matrixHeight = (matrixWidth / ratio);
+            }
+
+            var imageWidth = matrixWidth / 3;
+            var imageHeight = matrixHeight / 3;
+
+            console.log('Matrix width: ' + matrixWidth);
+            console.log('Matrix width: ' + matrixHeight);
+
+            $('.photo').width(imageWidth);
+            $('.photo').height(imageHeight);
         }
     };
 
